6f1a9ac89226ac6875263aab0df5c96919eb409d
kaitokid222 <33453552+kaitokid222@users.noreply.github.com>
2021-11-26 14:11:33 +0100

    Security & tidyness

--- Script/functions.php
+++ Script/functions.php
@@ -12,129 +12,129 @@
   }
 
   function update_users($id,$ip){
-	$newstring = "";
-	$player_is_in_file = 0;
+    $newstring = "";
+    $player_is_in_file = 0;
     $userfile = $GLOBALS["doc_root"] . "/Data/users.txt";
-	$userfile_data_string = file_get_contents($userfile);
-	$userfile_data = explode("|", $userfile_data_string);
-	if(count($userfile_data) > 0){
+    $userfile_data_string = file_get_contents($userfile);
+    $userfile_data = explode("|", $userfile_data_string);
+    if(count($userfile_data) > 0){
       foreach($userfile_data as $player){
-	    $t = explode(":",$player);
-		if($t[0] == $id){
-		  $player_is_in_file = 1;
-		  $t[1] = $ip;
-		}
-		if(!empty($t[0]) && !empty($t[1]))
-		  $newstring .= $t[0].":".$t[1]."|";
-	  }
-	}
-	if($player_is_in_file == 0){
-	  $newstring .= $id.":".$ip."|";
-	}
-	file_put_contents($userfile, $newstring);
+        $t = explode(":",$player);
+        if($t[0] == $id){
+          $player_is_in_file = 1;
+          $t[1] = $ip;
+        }
+        if(!empty($t[0]) && !empty($t[1]))
+          $newstring .= $t[0].":".$t[1]."|";
+      }
+    }
+    if($player_is_in_file == 0){
+      $newstring .= $id.":".$ip."|";
+    }
+    file_put_contents($userfile, $newstring);
   }
 
   function get_user_ip_from_id($id){
-	$r = "";
-	$userfile = $GLOBALS["doc_root"] . "/Data/users.txt";
-	$userfile_data_string = file_get_contents($userfile);
-	$userfile_data = explode("|", $userfile_data_string);
-	foreach($userfile_data as $player){
-	  $t = explode(":",$player);
-	  if($t[0] == $id){
-		$r = $t[1];
-		break;
-	  }
-	}
-	return $r;
+    $r = "";
+    $userfile = $GLOBALS["doc_root"] . "/Data/users.txt";
+    $userfile_data_string = file_get_contents($userfile);
+    $userfile_data = explode("|", $userfile_data_string);
+    foreach($userfile_data as $player){
+      $t = explode(":",$player);
+      if($t[0] == $id){
+        $r = $t[1];
+        break;
+      }
+    }
+    return $r;
   }
   
   function get_handshake_string($lobbyid){
-	$return_string = "";
-	$players = array();
-	$lobby = explode("|", lobby_to_string($lobbyid));
-	$i = 2;
-	while($i<6){
-	  if(!empty($lobby[$i])){
-	    $players[] = $lobby[$i];
-	  }else{
-	    break 1;
-	  }
-	  $i++;
-	}
-	$tarr = array();
-	foreach($players as $player){
-		$tarr[] = $player . ":" . get_user_ip_from_id($player);
-	}
-	$return_string = implode("|", $tarr);
-	return $return_string;
+    $return_string = "";
+    $players = array();
+    $lobby = explode("|", lobby_to_string($lobbyid));
+    $i = 2;
+    while($i<6){
+      if(!empty($lobby[$i])){
+        $players[] = $lobby[$i];
+      }else{
+        break 1;
+      }
+      $i++;
+    }
+    $tarr = array();
+    foreach($players as $player){
+        $tarr[] = $player . ":" . get_user_ip_from_id($player);
+    }
+    $return_string = implode("|", $tarr);
+    return $return_string;
   }
 
   function get_index(){
     $file = $GLOBALS["doc_root"] . "/Data/index.txt";
-	$file_data_string = file_get_contents($userfile);
-	$file_data_string = intval($file_data_string);
+    $file_data_string = file_get_contents($userfile);
+    $file_data_string = intval($file_data_string);
     return $file_data_string;
   }
 
   function get_next_index(){
     $i = get_index();
-	$i = $i+1;
+    $i = $i+1;
     return $i;
   }
 
   function create_lobby($s){
-	$lobbysting = "0|". $s;
-	$i = get_next_index();
-	file_put_contents($GLOBALS["doc_root"] . "/Data/lobby_" . $i . ".txt", $lobbysting);
-	file_put_contents($GLOBALS["doc_root"] . "/Data/index.txt", $i);
+    $lobbysting = "0|". $s;
+    $i = get_next_index();
+    file_put_contents($GLOBALS["doc_root"] . "/Data/lobby_" . $i . ".txt", $lobbysting);
+    file_put_contents($GLOBALS["doc_root"] . "/Data/index.txt", $i);
     return $i;
   }
 
   function join_lobby($lobbyid){
     $lobby = lobby_to_string($lobbyid);
-	$lobby_array = explode("|", $lobby);
-	$lobby_array[0] = intval($lobby_array[0])+1;
-	$lobby = implode("|", $lobby_array);
-	$lobby .= "|" . $GLOBALS["user_id"];
-	file_put_contents($GLOBALS["doc_root"] . "/Data/lobby_" . $lobbyid . ".txt", $lobby);
-	return $lobby;
+    $lobby_array = explode("|", $lobby);
+    $lobby_array[0] = intval($lobby_array[0])+1;
+    $lobby = implode("|", $lobby_array);
+    $lobby .= "|" . $GLOBALS["user_id"];
+    file_put_contents($GLOBALS["doc_root"] . "/Data/lobby_" . $lobbyid . ".txt", $lobby);
+    return $lobby;
   }
 
   function lobby_to_string($lobbyid){
     $lobbyfile = $GLOBALS["doc_root"] . "/Data/lobby_" . $lobbyid . ".txt";
-	$lobbyfile_data_string = file_get_contents($lobbyfile);
-	$return = $lobbyfile_data_string;
+    $lobbyfile_data_string = file_get_contents($lobbyfile);
+    $return = $lobbyfile_data_string;
     return $return;
   }
   
   function create_lobbylist(){
-	$result_array = array();
+    $result_array = array();
     $lobbycount = 0;
     $fileList = glob($GLOBALS["doc_root"] . "/Data/*.txt");
     foreach($fileList as $filename){
       if(is_file($filename)){
-		$tfn = explode("/", $filename);
-		$fn = end($tfn);
-	    if($fn[0] == "l"){
-		  $t = explode(".", $fn, 2);
-		  $tn = explode("_", $t[0], 2);
-		  $lobbyid = $tn[1];
-		  $result_array[] = get_lobby_player_count($lobbyid) . ":" . $lobbyid;
-		  $lobbycount++;
-		}
+        $tfn = explode("/", $filename);
+        $fn = end($tfn);
+        if($fn[0] == "l"){
+          $t = explode(".", $fn, 2);
+          $tn = explode("_", $t[0], 2);
+          $lobbyid = $tn[1];
+          $result_array[] = get_lobby_player_count($lobbyid) . ":" . $lobbyid;
+          $lobbycount++;
+        }
       }
-	}
-	$return = $lobbycount . "|" . implode("|", $result_array);
+    }
+    $return = $lobbycount . "|" . implode("|", $result_array);
     return $return;
   }
 
   function get_lobby_player_count($lobbyid){
     $lobbyfile = $GLOBALS["doc_root"] . "/Data/lobby_" . $lobbyid . ".txt";
-	$lobbyfile_data = file_get_contents($lobbyfile);
-	$lobbyfile_data_array = explode("|", $lobbyfile_data);
-	$playercount = $lobbyfile_data_array[0];
-	$return = $playercount;
+    $lobbyfile_data = file_get_contents($lobbyfile);
+    $lobbyfile_data_array = explode("|", $lobbyfile_data);
+    $playercount = $lobbyfile_data_array[0];
+    $return = $playercount;
     return $return;
   }
 
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
   $GLOBALS["doc_root"] = $_SERVER["DOCUMENT_ROOT"];
-  require("Script/functions.php");
+  require($GLOBALS["doc_root"] . "/Script/functions.php");
   
   //index.php?u=1234567890&a=1
   //index.php?u=1234567890&a=2
@@ -9,74 +9,96 @@
   //index.php?u=1234567890&a=4&l=12345
   //index.php?u=1234567890&a=5&l=12345
 
-  if(isset($_GET["u"]) && !empty($_GET["u"]))
-    $_GET["u"] = intval($_GET["u"]);
-  if(isset($_GET["l"]) && !empty($_GET["l"]))
-    $_GET["l"] = intval($_GET["l"]);
-  if(isset($_GET["s"]) && !empty($_GET["s"]))
-    $_GET["s"] = intval($_GET["s"]);
-  if(isset($_GET["a"]) && !empty($_GET["a"]))
-    $_GET["a"] = intval($_GET["a"]);
+  //Check Input
+  if(!isset($_GET["u"]) || empty($_GET["u"])){
+    $output = "Missing Input User ID";
+    echo $output;
+    die();
+  }else{
+    if(!preg_match("#^[0-9]+$#", $_GET["u"])){
+      $output = "Input User ID is not numeric";
+      echo $output;
+      die();
+    }else{
+      $_GET["u"] = intval($_GET["u"]);
+    }
+  }
 
-  if(!is_int($_GET["u"])){
-    $output = "No numeric user id!";
+  if(!isset($_GET["a"]) || empty($_GET["a"])){
+    $output = "Missing Input Action";
+    echo $output;
+    die();
+  }else{
+    if(!preg_match("#^[0-9]+$#", $_GET["a"])){
+      $output = "Input Action is not numeric";
+      echo $output;
+      die();
+    }else{
+      $_GET["a"] = intval($_GET["a"]);
+    }
+  }
+
+  if($_GET["a"] == 3){
+    if(!isset($_GET["s"]) || empty($_GET["s"])){
+      $output = "Missing Input Gamemode";
+      echo $output;
+      die();
+    }else{
+      if(!preg_match("#^[0-9]+$#", $_GET["s"])){
+        $output = "Input Gamemode is not numeric";
+        echo $output;
+        die();
+      }else{
+        $_GET["s"] = intval($_GET["s"]);
+      }
+    }
+  }
+
+  if($_GET["a"] == 4 || $_GET["a"] == 5){
+    if(!isset($_GET["l"]) || empty($_GET["l"])){
+      $output = "Missing Input Lobby ID";
+      echo $output;
+      die();
+    }else{
+      if(!preg_match("#^[0-9]+$#", $_GET["l"])){
+        $output = "Input Lobby ID is not numeric";
+        echo $output;
+        die();
+      }else{
+        $_GET["l"] = intval($_GET["l"]);
+      }
+    }
+  }
+
+  if($_GET["a"] <= 0 || $_GET["a"] >= 6){
+    $output = "Unknown Request!";
     echo $output;
     die();
   }
-  $user_id = $_GET["u"];
-  $GLOBALS["user_id"] = $user_id;
+  // $_GET is trustworthy now
+
+  $GLOBALS["user_id"] = $_GET["u"];
   $user_ip = get_user_ip();
-  $users = update_users($user_id, $user_ip);
+  $users = update_users($_GET["u"], $user_ip);
+
   
   if($_GET["a"] == 1 || $_GET["a"] == 2){
     $action = "listlobbies";
-  }
-  elseif($_GET["a"] == 3)
-  {
+  }elseif($_GET["a"] == 3){
     $action = "createlobby";
-	$gamemode = 0;
-	if(is_int($_GET["s"])){
-	  $gamemode = $_GET["s"];
-	}
-	if($gamemode == 0){
-      $output = "Missing Input Gamemode!";
-      echo $output;
-      die();
-	}
-  }
-  elseif($_GET["a"] == 4)
-  {
+    $gamemode = $_GET["s"];
+  }elseif($_GET["a"] == 4){
     $action = "joinlobby";
-	$lobbyid = 0;
-	if(is_int($_GET["l"])){
-	  $lobbyid = $_GET["l"];
-	}
-	if($lobbyid == 0){
-      $output = "Missing Input Lobbyid!";
-      echo $output;
-      die();
-	}
-  }
-  elseif($_GET["a"] == 5)
-  {
-	$action = "startgame";
-	$lobbyid = 0;
-	if(is_int($_GET["l"])){
-	  $lobbyid = $_GET["l"];
-	}
-	if($lobbyid === 0){
-      $output = "Missing Input Lobbyid!";
-      echo $output;
-      die();
-	}
-  }else{
-	die();
+    $lobbyid = $_GET["l"];
+  }elseif($_GET["a"] == 5){
+    $action = "startgame";
+    $lobbyid = $_GET["l"];
   }
  
   if($action == "listlobbies"){
     $output = create_lobbylist();
   }elseif($action == "createlobby"){
-	$lid = create_lobby($gamemode);
+    $lid = create_lobby($gamemode);
     $output = join_lobby($lid);
   }elseif($action == "startgame"){
     $output = get_handshake_string($lobbyid);
